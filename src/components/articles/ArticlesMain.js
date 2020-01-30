918897e4c4ab56787c1bf3272006893260c15029
varunon9 <varunon9@gmail.com>
2020-01-30 23:23:32 +0530

    converting ArticlesMain into hook based functional component

--- src/components/articles/ArticlesMain.js
+++ src/components/articles/ArticlesMain.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import { Link } from 'react-router-dom';
 
 import ArticleListItem from './ArticleListItem';
@@ -65,4 +65,62 @@ class ArticlesMain extends React.Component {
   }
 }
 
+export default ArticlesMain;*/
+
+// Hook based component
+
+import React, { useState, useRef, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+import ArticleListItem from './ArticleListItem';
+import '../../css/ArticlesMain.css';
+import { getArticles } from '../../actions/ApiClient';
+import customLocalStorage from '../../utils/customLocalStorage';
+
+const ArticlesMain = props => {
+  const [articles, setArticles] = useState([]);
+  const user = useRef(customLocalStorage.getItem('user')).current;
+
+  useEffect(() => {
+    getArticles()
+      .then(response => setArticles(response))
+      .catch(error => console.log(error));
+    }, []);
+
+  return (
+    <div className="ui segment basic">
+      <div className="ui equal width grid">
+        <div className=" column">
+          Hello { user ? user.name : 'Guest'}
+        </div>
+        <div className="column">
+          <div className="ui secondary menu">
+            <div className="right menu">
+              {
+                user ?
+                  <>
+                    <Link to="/articles/create" className="item">Create an article</Link>
+                    <Link to="/logout" className="item">Logout</Link>
+                  </>
+                  :
+                  <>
+                    <Link to="/login" className="item">Login</Link>
+                  </>
+              }
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="ui header">Articles</div>
+      <div className="ui feed">
+        {
+          articles.map((article, index) => (
+            <ArticleListItem key={article.id} article={article} user={user} />
+          ))
+        }
+      </div>
+    </div>
+  );
+};
 export default ArticlesMain;
+
